0842942cc37827f4994f8b20faeef35dde5ad247
Joe <joeparkkennaby@gmail.com>
2021-04-01 15:17:09 +0100

    9 - update postgres setup. change package version + initial table setup and error logging in server application

--- server/index.js
+++ server/index.js
@@ -18,12 +18,11 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort,
 });
+pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient.on('connect', () => {
-  pgClient
-    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((err) => console.log(err));
-});
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch(err => console.log(err));
 
 // Redis Client Setup
 const redis = require('redis');
