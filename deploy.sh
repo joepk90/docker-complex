0ab96348c29002796cc71714b7c54935a524d16b
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-10-25 16:42:47 +0100

    16.25 - update the bash deployment script to use the git sha environment variable. update the script to tag each image twice -  latest + git sha. update kublectl image deployment to set each image using git sha

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-docker build -t jparkkennaby/complex-client -f ./client/Dockerfile ./client
-docker build -t jparkkennaby/complex-server -f ./server/Dockerfile ./server
-docker build -t jparkkennaby/complex-worker -f ./worker/Dockerfile ./worker
+docker build -t jparkkennaby/complex-client:latest -t jparkkennaby/complex-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jparkkennaby/complex-server:latest -t jparkkennaby/complex-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jparkkennaby/complex-worker:latest -t jparkkennaby/complex-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push jparkkennaby/complex-client
-docker push jparkkennaby/complex-server
-docker push jparkkennaby/complex-worker
+docker push jparkkennaby/complex-client:latest
+docker push jparkkennaby/complex-server:latest
+docker push jparkkennaby/complex-worker:latest
+
+docker push jparkkennaby/complex-client:$SHA
+docker push jparkkennaby/complex-server:$SHA
+docker push jparkkennaby/complex-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=jparkkennaby/complex-server
+kubectl set image deployments/server-deployment server=jparkkennaby/complex-server:$SHA
+kubectl set image deployments/client-deployment client=jparkkennaby/complex-client:$SHA
+kubectl set image deployments/worker-deployment worker=jparkkennaby/complex-worker:$SHA
